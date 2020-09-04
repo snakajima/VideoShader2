474d57f058d803cc746bc2ab5c21e0bb9e1cef6c
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-04 05:08:05 -0700

    Initial Commit

---
+++ vs2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vs2
+//
+//  Created by SATOSHI NAKAJIMA on 9/4/20.
+//  Copyright © 2020 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
