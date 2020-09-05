e5cfd81a09ff69f625f75e552b4cca5584f34702
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-04 22:01:19 -0700

    interim

--- vs2/VS2CameraViewController.swift
+++ vs2/VS2CameraViewController.swift
@@ -17,21 +17,33 @@ final class VS2CameraViewController: UIViewController {
 
     override func viewDidLoad() {
         cameraSession.startRunning()
-        let size = CGSize(width:cameraSession.dimension.width, height:cameraSession.dimension.height)
-        metalView.frame = CGRect(origin: .zero, size: size)
         metalView.device = self.cameraSession.gpu
         metalView.delegate = self
         metalView.clearColor = MTLClearColorMake(1, 1, 1, 1)
         metalView.colorPixelFormat = MTLPixelFormat.bgra8Unorm
         metalView.framebufferOnly = false
         view.addSubview(metalView)
+        
+        self.viewDidLayoutSubviews()
     }
     
     override func viewDidLayoutSubviews() {
+        let scale = UIScreen.main.scale
+        let size = CGSize(width:cameraSession.dimension.width, height:cameraSession.dimension.height)
+        metalView.frame = CGRect(origin: .zero, size: size)
+        metalView.drawableSize = size
+        /*
+        let ratio = min(view.frame.size.width / size.width, view.frame.size.height / size.height)
+        print(ratio)
+        let xfScale = CATransform3DMakeScale(ratio, ratio, 1.0)
+        let xfTranslate = CATransform3DMakeTranslation(0, -size.height / 2 * (1-ratio), 0)
+        metalView.layer.transform = CATransform3DConcat(xfTranslate, xfScale)
+        */
+        /*
         let scale = min(view.frame.size.width / metalView.frame.size.width, view.frame.size.height / metalView.frame.size.height)
-            * UIScreen.main.scale
         print(scale)
-        metalView.layer.transform = CATransform3DMakeScale(scale, scale, 1.0)
+        metalView.layer.transform =
+        */
     }
 }
 
