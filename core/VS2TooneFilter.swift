a6d42248de67ed53984520465193b780260112eb
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-08 10:03:51 -0700

    code written

---
+++ core/VS2TooneFilter.swift
@@ -0,0 +1,32 @@
+//
+//  VS2TooneFilter.swift
+//  vs2
+//
+//  Created by SATOSHI NAKAJIMA on 9/8/20.
+//  Copyright © 2020 SATOSHI NAKAJIMA. All rights reserved.
+//
+
+import Foundation
+import CoreImage
+
+class VS2TooneFilter: CIFilter {
+    private let kernel: CIColorKernel
+
+    var inputImage: CIImage?
+
+    override init() {
+        let url = Bundle.main.url(forResource: "default", withExtension: "metallib")!
+        let data = try! Data(contentsOf: url)
+        kernel = try! CIColorKernel(functionName: "myColor", fromMetalLibraryData: data)
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func outputImage() -> CIImage? {
+        guard let inputImage = inputImage else {return nil}
+        return kernel.apply(extent: inputImage.extent, arguments: [inputImage])
+    }
+}
--- macOS/ContentView.swift
+++ macOS/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 let s_script0 = [
     "pipeline":[[
-        "filter": "colorClamp",
+        "filter": "toone",
         "props":[
             "minComponents":[0.0, 0.0, 0.0, 0.0],
             "maxComponents":[0.5, 1.0, 1.0, 1.0]
