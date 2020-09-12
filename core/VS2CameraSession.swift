5e01f4953b19e5494042a8bb64d83629f1ab6f41
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-12 08:01:37 -0700

    source over

--- core/VS2CameraSession.swift
+++ core/VS2CameraSession.swift
@@ -149,9 +149,9 @@ class VS2CameraSession: NSObject {
         pipeline.encode(commandBuffer: commandBuffer, ciImageSrc: filterScale.outputImage!)
         
         if let ciImageShape = ciImageShape {
-            if let filter = CIFilter(name: "CIAdditionCompositing") {
-                filter.setValue(ciImageShape, forKey: kCIInputBackgroundImageKey)
-                filter.setValue(pipeline.pop(), forKey: kCIInputImageKey)
+            if let filter = CIFilter(name: "CISourceOverCompositing") {
+                filter.setValue(ciImageShape, forKey:  kCIInputImageKey )
+                filter.setValue(pipeline.pop(), forKey: kCIInputBackgroundImageKey)
                 pipeline.push(filter.outputImage)
             }
         }
