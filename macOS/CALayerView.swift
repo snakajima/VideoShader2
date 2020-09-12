58b171e1d0f42a919769a20359897f875cc382c6
Satoshi Nakajima <satoshi.nakajima@gmail.com>
2020-09-12 04:11:00 -0700

    animation

--- macOS/CALayerView.swift
+++ macOS/CALayerView.swift
@@ -24,12 +24,43 @@ struct CALayerView: NSViewRepresentable {
     
     func updateNSView(_ view: NSView, context: NSViewRepresentableContext<CALayerView>) {
         if let layer = view.layer as? CAShapeLayer {
-            let path = CGMutablePath()
-            path.move(to: CGPoint(x:0, y:0))
-            path.addLine(to: CGPoint(x: 100, y: 100))
-            path.addLine(to: CGPoint(x: 0, y: 100))
-            path.closeSubpath()
-            layer.path = path
+            let starPath = CGMutablePath()
+            starPath.move(to: CGPoint(x: 81.5, y: 7.0))
+            starPath.addLine(to: CGPoint(x: 101.07, y: 63.86))
+            starPath.addLine(to: CGPoint(x: 163.0, y: 64.29))
+            starPath.addLine(to: CGPoint(x: 113.16, y: 99.87))
+            starPath.addLine(to: CGPoint(x: 131.87, y: 157.0))
+            starPath.addLine(to: CGPoint(x: 81.5, y: 122.13))
+            starPath.addLine(to: CGPoint(x: 31.13, y: 157.0))
+            starPath.addLine(to: CGPoint(x: 49.84, y: 99.87))
+            starPath.addLine(to: CGPoint(x: 0.0, y: 64.29))
+            starPath.addLine(to: CGPoint(x: 61.93, y: 63.86))
+            starPath.addLine(to: CGPoint(x: 81.5, y: 7.0))
+            
+            let rectanglePath = CGMutablePath()
+            rectanglePath.move(to: CGPoint(x: 81.5, y: 7.0))
+            rectanglePath.addLine(to: CGPoint(x: 163.0, y: 7.0))
+            rectanglePath.addLine(to: CGPoint(x: 163.0, y: 82.0))
+            rectanglePath.addLine(to: CGPoint(x: 163.0, y: 157.0))
+            rectanglePath.addLine(to: CGPoint(x: 163.0, y: 157.0))
+            rectanglePath.addLine(to: CGPoint(x: 82.0, y: 157.0))
+            rectanglePath.addLine(to: CGPoint(x: 0.0, y: 157.0))
+            rectanglePath.addLine(to: CGPoint(x: 0.0, y: 157.0))
+            rectanglePath.addLine(to: CGPoint(x: 0.0, y: 82.0))
+            rectanglePath.addLine(to: CGPoint(x: 0.0, y: 7.0))
+            rectanglePath.addLine(to: CGPoint(x: 81.5, y: 7.0))
+            
+            layer.path = starPath
+            
+            let pathAnimation = CABasicAnimation(keyPath: "path")
+            pathAnimation.toValue = rectanglePath
+            pathAnimation.duration = 0.75
+            pathAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+            pathAnimation.autoreverses = true
+            pathAnimation.repeatCount = .greatestFiniteMagnitude
+
+            layer.add(pathAnimation, forKey: "pathAnimation")
+            
             layer.strokeColor = NSColor.red.cgColor
             layer.fillColor = NSColor.green.cgColor
         }
